0b70e5f0a10893c72d65abbf27ecef809b4aa097
Melissa Arroyo <melissaarroyotorres9806@gmail.com>
2023-05-09 12:38:41 -0400

    adding branch

---
+++ coffee_shop-main/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:coffee_shop/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFF212325),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
---
+++ coffee_shop-main/lib/screens/home_screen.dart
@@ -0,0 +1,138 @@
+import 'package:coffee_shop/widgets/home_bottom_bar.dart';
+import 'package:coffee_shop/widgets/items_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  @override
+  void initState() {
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 0);
+    _tabController.addListener(_handleTabSelection);
+    super.initState();
+  }
+
+  _handleTabSelection() {
+    if (_tabController.indexIsChanging) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(top: 15),
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.sort_rounded,
+                        color: Colors.white.withOpacity(0.5),
+                        size: 35,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.notifications,
+                        color: Colors.white.withOpacity(0.5),
+                        size: 35,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Text(
+                  "It's a Great Day for Coffee",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                width: MediaQuery.of(context).size.width,
+                height: 60,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 50, 54, 56),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Find ypur Coffee",
+                    hintStyle: TextStyle(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      size: 30,
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+              ),
+              TabBar(
+                controller: _tabController,
+                labelColor: Color(0xFFE57734),
+                unselectedLabelColor: Colors.white.withOpacity(0.5),
+                isScrollable: true,
+                indicator: UnderlineTabIndicator(
+                  borderSide: BorderSide(
+                    width: 3,
+                    color: Color(0xFFE57734),
+                  ),
+                  insets: EdgeInsets.symmetric(horizontal: 16),
+                ),
+                labelStyle:
+                    TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                labelPadding: EdgeInsets.symmetric(horizontal: 20),
+                tabs: [
+                  Tab(text: "Latte"),
+                  Tab(text: "Expresso"),
+                  Tab(text: "Macchiato"),
+                  Tab(text: "Capuiccino"),
+                ],
+              ),
+              SizedBox(height: 10),
+              Center(
+                child: [
+                  ItemsWidget(),
+                  ItemsWidget(),
+                  ItemsWidget(),
+                  ItemsWidget(),
+                ][_tabController.index],
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: HomeBottomBar(),
+    );
+  }
+}
