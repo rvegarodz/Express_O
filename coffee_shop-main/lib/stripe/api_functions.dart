c10214b20ef2118e16d87796154d372a36e13af1
rvegarodz <r.vega27@outlook.com>
2023-07-03 10:20:11 -0400

    open payment in same page

--- coffee_shop-main/lib/stripe/api_functions.dart
+++ coffee_shop-main/lib/stripe/api_functions.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'dart:async';
-import 'package:url_launcher/url_launcher.dart';
+import 'dart:html';
 import 'package:http/http.dart' as http;
 
 /// Function that make a POST request before launching new tab
@@ -26,19 +26,26 @@ Future<void> createPaymentIntentAndRedirect(int total) async {
   // Verifying POST Status
   print('Response status code: ${response.statusCode}');
   if (response.statusCode == 200) {
-    final jsonData = json.decode(response.body);
-    final paymentIntentID = jsonData['paymentIntentID'];
+    // The following lines are for Business Owner Side :
+    // |--------------------------------------------------------|
+    // |  final jsonData = json.decode(response.body);          |
+    // |  final paymentIntentID = jsonData['paymentIntentID'];  |
+    // |--------------------------------------------------------|
 
-    // Launch the payment URL in new tab
-    await launchUrl(urlPayment);
+    // Open the payment URL in the same tab/window
+    window.location.href = urlPayment.toString();
 
-    // Check if the payment URL
-    retrievePaymentIntentWithRetry(paymentIntentID);
+    // Check if the payment process was successful,
+    // to trigger other actions as display order to Business Owner Screen:
+    // |--------------------------------------------------------|
+    // |  retrievePaymentIntentWithRetry(paymentIntentID);      |
+    // |--------------------------------------------------------|
   } else {
     throw Exception('Failed to create payment intent');
   }
 }
 
+// At the moment is not in use.
 Future<void> retrievePaymentIntentWithRetry(String paymentIntentID) async {
   final maxRetryAttempts = 10;
   final retryDelay = Duration(seconds: 40);
