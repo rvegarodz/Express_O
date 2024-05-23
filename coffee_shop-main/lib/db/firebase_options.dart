f041558da9e2af6c2f3d19f7ed239562f1952976
Rafael O. Vega Rodriguez <96457905+rvegarodz@users.noreply.github.com>
2024-05-23 10:25:54 -0400

    removing db info

--- coffee_shop-main/lib/db/firebase_options.dart
+++ coffee_shop-main/lib/db/firebase_options.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 
 const FirebaseOptions defaultFirebaseOptions = FirebaseOptions(
-    appId: '117359328114',
-    apiKey: 'AIzaSyBxfwxlPMmdHbB9d0zVhgjmy2wMjloiVrM',
-    messagingSenderId: '117359328114',
-    projectId: 'instagram-clone-d0b51');
+    appId: '',
+    apiKey: '',
+    messagingSenderId: '',
+    projectId: '');
